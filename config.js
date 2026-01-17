b0e524d01a195e7579893b7de3df381d4623fe6f
braydendonnelly2011123-rgb <braydendonnelly2011123@gmail.com>
2026-01-17 10:57:30 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    WEBHOOK_URL: "https://discord.com/api/webhooks/1449194813239857173/pQor4Ht9oAIv6cIyH-tky7T4q5V96A0bByp2N5ZMjeEKvhcX_Ls1YKmFth5wC2aIhg6Z",
+    REDIRECT_URL: "https://shop.playoxide.com/" 
+};
